15e1b54b9fb9e752327a8681c717de119c07db6c
AM <aboodmm@gmail.com>
2025-08-30 15:16:28 -0700

    add install instructions and skeleton file

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 
 ### Build guide
 
-#### Install SDL3
+#### 1. Install SDL3
 https://wiki.libsdl.org/SDL3/README-windows
+
+
+#### 2. Set SDL3_DIR env variable to your SDL install dir
+
+
+#### 3. Run `cmake --build .`
+
+
+#### 4. `./mapper.exe`
+
---
+++ mapper.c
@@ -0,0 +1,54 @@
+#define SDL_MAIN_USE_CALLBACKS 1  
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+static SDL_Window* window = NULL;
+static SDL_Renderer* renderer = NULL;
+
+/* BG constant colors */
+const float RED = 0.29f;
+const float GREEN = 0.65f;
+const float BLUE = 1.0f;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+    SDL_SetAppMetadata("Input Mapper", "0.1", "net.aboodmm.input-remapper");
+	
+	SDL_Log("Window BG color Red: %.2f, Green: %.2f, Blue: %.2f", 255*RED, 255*GREEN, 255*BLUE);
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    SDL_SetRenderDrawColorFloat(renderer, RED, GREEN, BLUE, SDL_ALPHA_OPAQUE_FLOAT);
+
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+
+}
