b7a89172a5a59ae653804a3171f68d79dafd3799
AM <aboodmm@gmail.com>
2025-12-07 10:21:52 -0800

    include sdl_image for drawing pngs

--- README.md
+++ README.md
@@ -2,15 +2,27 @@
 
 ### Build guide
 
+This install guide will be aimed at MinGW users. 
+
 #### 1. Install SDL3
 https://wiki.libsdl.org/SDL3/README-windows
 tip (mingw) Install this MinGW package `mingw-w64-x86_64-toolchain` to install prereqs
+
+#### 2. Install SDL3_image
+
+https://github.com/libsdl-org/SDL_image/releases
+
+On MinGW, download lib, extract tarball, and run `make`. 
+
+Follow their instructions.
+
 
 #### 3. Set SDL3_DIR env variable to your SDL install dir
 
 
 #### 4. Run `cmake --build .`
 
+#### 5. Run my executable to launch the program
 
-#### 5. `./mapper.exe`
+`./mapper.exe`
 
--- mapper.c
+++ mapper.c
@@ -1,6 +1,9 @@
-#define SDL_MAIN_USE_CALLBACKS 1  
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <stdio.h> 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3/SDL_iostream.h>
 
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
@@ -10,8 +13,10 @@ const float RED = 0.29f;
 const float GREEN = 0.65f;
 const float BLUE = 1.0f;
 
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
-{
+/*
+	Initialize SDL App, create window and renderer
+*/
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     SDL_SetAppMetadata("Input Mapper", "0.1", "net.aboodmm.input-remapper");
 	
 	SDL_Log("Window BG color Red: %.2f, Green: %.2f, Blue: %.2f", 255*RED, 255*GREEN, 255*BLUE);
@@ -29,16 +34,15 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
-{
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  
     }
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void* appstate)
-{
+SDL_AppResult SDL_AppIterate(void* appstate) {
     SDL_SetRenderDrawColorFloat(renderer, RED, GREEN, BLUE, SDL_ALPHA_OPAQUE_FLOAT);
 
     SDL_RenderClear(renderer);
@@ -48,7 +52,30 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void* appstate, SDL_AppResult result)
-{
+/*
+	Load the initial button from png file
+*/
+bool loadImage() {
+    bool success = true;
+	
+	const char read_mode = 'r';
+
+    SDL_Surface* image = IMG_LoadPNG_IO(SDL_IOFromFile("img/botan.png", &read_mode));
+    if( image == NULL )
+    {
+        printf( "Failed %s!", SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+/*
+	Recolor buttons and arrange them like fightstick
+*/
+void transformButtons() {
+	return;
+}
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
 
 }
